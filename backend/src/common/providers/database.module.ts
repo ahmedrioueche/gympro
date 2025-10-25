f459972dbd9243b394faffbb142e2bc726973ba1
Ahmed Drioueche <adsrahmed@gmail.com>
2025-10-26 00:19:51 +0100

    removed backend

--- backend/src/common/providers/database.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { mongoProvider } from './mongo.provider';
-
-@Module({
-  providers: [mongoProvider],
-  exports: [mongoProvider],
-})
-export class DatabaseModule {}
