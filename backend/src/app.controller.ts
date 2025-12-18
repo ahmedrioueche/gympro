b2c2586bd6e0027566814bbde6e4896c2c7d15dc
Ahmed Drioueche <adsrahmed@gmail.com>
2025-12-18 19:59:29 +0100

    wake up cron job

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -2,8 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get()
-  getHello(): string {
-    return 'Hello World!';
+  @Get('wake-up')
+  wake() {
+    return {
+      status: 'awake',
+      timestamp: new Date().toISOString(),
+    };
   }
 }
